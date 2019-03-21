7db4b8b6a2e2852befffa28f134195a5cccaaf1d
zeeecccooo <48753839+zeeecccooo@users.noreply.github.com>
2019-03-21 23:11:15 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,7 +14,7 @@ client.on('ready', () => {
     console.log(`in ${client.guilds.size} servers `)
     console.log(`[Codes] ${client.users.size}`)
 });
-const prefix = "3"
+const prefix = "$"
 client.on('message', async msg => {
     if (msg.author.bot) return undefined;
     if (!msg.content.startsWith(prefix)) return undefined;
